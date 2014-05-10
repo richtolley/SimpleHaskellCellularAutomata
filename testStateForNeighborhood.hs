51d4d775892a1e483289134e757d4e7727d88fa7
Rich Tolley <rich9tolley@gmail.com>
2014-05-10 16:29:58 +0100

    removed build product files

--- testStateForNeighborhood.hs
+++
@@ -1,9 +0,0 @@
---test for stateForNeighborhood method
-
-stateForNeighborhood [0,0,0]
-stateForNeighborhood [0,1,0]
-stateForNeighborhood [0,1,1]
-stateForNeighborhood [1,0,0]
-stateForNeighborhood [1,0,1]
-stateForNeighborhood [1,1,0]
-stateForNeighborhood [1,1,1]
